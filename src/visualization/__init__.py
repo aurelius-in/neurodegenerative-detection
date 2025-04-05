55c5f87bd33b35a434abeb7f5ea36ef265423b58
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2025-04-05 15:25:17 -0500

    Create __init__.py

---
+++ src/visualization/__init__.py
@@ -0,0 +1,7 @@
+# src/visualization/__init__.py
+
+# Importing the plot_data function from the visualize module
+from .visualize import plot_data
+
+# Defining the public interface of the visualization package
+__all__ = ['plot_data']
